89158f5dfee935a8603ce13f09af9bc07c6cb30c
Aaron Bushnell <aabushnell@gmail.com>
2023-09-10 21:10:39 +0200

    basic dijkstra

--- grid_builder.py
+++ grid_builder.py
@@ -7,8 +7,9 @@ Dijkstra's Implementation
 """
 
 # import block
-import numpy as np
+from itertools import product
 
+import numpy as np
 from haversine import haversine
 from scipy.sparse import dok_matrix
 from scipy.sparse.csgraph import dijkstra
@@ -17,10 +18,11 @@ import cost_functions as cf
 import grid_helpers as gh
 
 
-class elevationGrid:
+class ElevationGrid:
 
-    def __init__(self, elevation_array, cell_size,
-                 starting_latitude, starting_longitude):
+    def __init__(self, elevation_array: np.ndarray[np.ndarray[int]],
+                 cell_size: float, starting_latitude: float,
+                 starting_longitude: float):
         self.elev_arr = elevation_array
         self.cell_size = cell_size
         self.lat_start = starting_latitude
@@ -32,54 +34,45 @@ class elevationGrid:
 
         self.node_ids = (np.arange(0, self.cell_count)
                          .reshape(self.lat_count, self.lon_count))
+        self.cost_mat = None
 
-    def get_travel_cost(self, start_y, start_x, end_y, end_x):
-        """
-
-        :param int start_y:
-        :param int start_x:
-        :param int end_y:
-        :param int end_x:
-        :return: float
-        """
-
+    def travel_cost(self, start_y: int, start_x: int,
+                    end_y: int, end_x: int) -> float:
         start_elev = self.elev_arr[start_y, start_x]
         end_elev = self.elev_arr[end_y, end_x]
 
         if start_elev != np.nan and end_elev != np.nan:
-
-            start_coord = (self.lat_start - self.cell_size * (start_y + 1/2),
-                           self.lon_start + self.cell_size * (start_x - 1/2))
-            end_coord = (self.lat_start - self.cell_size * (end_y + 1/2),
-                           self.lon_start + self.cell_size * (end_x - 1/2))
-
+            start_coord = gh.index_to_coord(start_y, start_x, self.cell_size,
+                                            lat_start=self.lat_start,
+                                            lon_start=self.lon_start,
+                                            center=True)
+            end_coord = gh.index_to_coord(end_y, end_x, self.cell_size,
+                                          lat_start=self.lat_start,
+                                          lon_start=self.lon_start,
+                                          center=True)
             distance = haversine(start_coord, end_coord)
-
             cost = cf.transport_method_cost(distance, start_elev, end_elev)
             return cost
-        else: return 0
+        else:
+            raise ValueError
 
-    def get_cost_matrix(self):
-        """
+    def cost_matrix(self) -> dok_matrix:
+        cost_mat = dok_matrix((self.cell_count, self.cell_count))
 
-        :return:
-        """
+        for i, j in product(range(0, self.lat_count),
+                            range(0, self.lon_count)):
+            id_orig = self.node_ids[i, j]
+            for di, dj in gh.get_neighbors(i, j, self.lat_count,
+                                           self.lon_count):
+                cost_mat[id_orig, self.node_ids[i + di, j + dj]] = (
+                    self.travel_cost(i, j, i + di, j + dj)
+                )
 
-        dist_mat = dok_matrix((self.cell_count, self.cell_count))
+        self.cost_mat = cost_mat
+        return cost_mat
 
-        for i in range(0, self.lon_count):
-            for j in range(0, self.lat_count):
-                id_orig = self.node_ids[i, j]
-                for di, dj in gh.get_neighbors(i, j, self.lon_count,
-                                               self.lat_count):
-                    dist_mat[id_orig, self.node_ids[i + di, j + dj]] = \
-                        self.get_travel_cost(i, j, i + di, j + dj)
-
-        self.dist_mat = dist_mat
-        return dist_mat
-
-
-
-
-
-
+    # noinspection PyTupleAssignmentBalance
+    def eval_dijkstra(self):
+        trav_time, pred = dijkstra(self.cost_mat, directed=False,
+                                   return_predecessors=True)
+        return trav_time, pred
