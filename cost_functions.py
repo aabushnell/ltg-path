099a310b2c349b081f335b41b15ac99eb4699d6a
Aaron Bushnell <aabushnell@gmail.com>
2023-09-29 16:56:14 +0200

    update cost function units

--- cost_functions.py
+++ cost_functions.py
@@ -3,32 +3,41 @@ TODO
 """
 
 coeffs = {
-    'flat': 0.72,
-    'up': 6,
-    'down_mod': 2,
-    'down_steep_cutoff': -0.2125,
-    'down_steep': -2,
-    'sea': 0.9,
-    'loading': 3.6,
-    }
+    'flat': 0.72,  # meters/second
+    'up': 6,  # meters/hour
+    'down_mod': 2,  # meters/hour
+    'down_steep_cutoff': -0.2125,  # angle
+    'down_steep': -2,  # meters/hour
+    'sea': 0.9,  # meters/sec
+    'loading': 1,  # equivalent to 1 hr of transport
+    'river': 0.9,  # meters/sec
+}
+
+UNIT_CONV = 1000 / 3600  # km/h to m/s
 
 
 def transport_method_cost(distance: float,
                           elevation_start: int,
-                          elevation_end: int) -> float:
+                          elevation_end: int,
+                          river_travel: bool) -> float:
     """
     Calculates the total 'cost' of moving along a given distance
     modified by predefined parameters and taking into account
     elevation change and implicit movement from land to sea
     (where any negative elevation is considered to be sea).
+    Also features a special cost for travel between adjacent
+    river nodes.
     """
     if elevation_start >= 0 and elevation_end >= 0:
-        cost = coeffs['flat'] * distance + elevation_change_cost(
-            elevation_start, elevation_end)
+        if river_travel:
+            cost = coeffs['river'] * coeffs['flat'] * distance * UNIT_CONV
+        else:
+            cost = (coeffs['flat'] * distance * UNIT_CONV
+                    + elevation_change_cost(elevation_start, elevation_end))
     elif elevation_start < 0 and elevation_end < 0:
-        cost = coeffs['sea'] * coeffs['flat'] * distance
+        cost = coeffs['sea'] * coeffs['flat'] * distance * UNIT_CONV
     else:
-        cost = coeffs['loading'] + coeffs['flat'] * distance
+        cost = coeffs['loading'] + coeffs['flat'] * distance * UNIT_CONV
     return cost
 
 
