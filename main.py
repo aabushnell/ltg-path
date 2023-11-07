fe8cfad1aa6ff81052ffbd72c9035c8a82d9e736
Aaron Bushnell <aabushnell@gmail.com>
2023-11-07 11:01:10 +0100

    reformat main

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import grid_helpers as gh
 import file_reader as fr
 import rasterize_rivers as rr
 
-CELL_SIZE = 1/12
+CELL_SIZE = 1 / 12
 
 elev_full = fr.read_elevation_full()
 river_full = rr.read_rivers_raster(cellsize=CELL_SIZE)
-full_elev_grid = gb.ElevationGrid(elev_full, cell_size=CELL_SIZE,
+full_elev_grid = gb.ElevationGrid(-1, elev_full, cell_size=CELL_SIZE,
                                   lat_start=0, lon_start=0,
                                   river_array=river_full)
 
@@ -17,22 +17,29 @@ full_elev_grid = gb.ElevationGrid(elev_full, cell_size=CELL_SIZE,
 
 # full_elev_grid.mask_deep_sea(3)
 
-COARSE_DIM = 3
+COARSE_DIM = 24
 # Y_START = 626
 # X_START = 1466
 
-Y_START = 490
+Y_START = 480
 X_START = 20
 
-y_end = Y_START + COARSE_DIM*3 - 1
-x_end = X_START + COARSE_DIM*3 - 1
+print(gh.index_to_coord(Y_START, X_START, CELL_SIZE))
 
-grid = full_elev_grid.subgrid(Y_START, X_START, y_end, x_end)
+y_end = Y_START + COARSE_DIM * 3 - 1
+x_end = X_START + COARSE_DIM * 3 - 1
+
+grid = full_elev_grid.subgrid(1, Y_START, X_START, y_end, x_end)
 
 print(grid.grid)
 print(grid.elev_arr)
-print(grid.river_arr)
+grid.mask_deep_sea(3)
+print(grid.elev_arr_deep)
+
+grid.build_cost_matrix()
+out = grid.neigbor_grid_output(flat_inner_matrix=True)
+print(out)
 
 
-# grid.build_cost_matrix()
-# print(grid.neigbor_grid_output(flat_inner_matrix=True))
+costs = out.uniform_weights(grid.elev_arr)
+print(costs)
